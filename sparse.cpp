bf9f9a5346104289b192ebcce2a5d579cd80280f
Eric Schiller <eschiller@users.noreply.github.com>
2016-08-01 09:43:02 -0400

    fixing tabs

--- sparse.cpp
+++ sparse.cpp
@@ -11,11 +11,11 @@ int main(int ac, char *av[]) {
 	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
 	//make window black
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
 
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderDrawPoint(renderer, 960, 540); 
   	SDL_RenderPresent(renderer);
 	std::cin.ignore();
